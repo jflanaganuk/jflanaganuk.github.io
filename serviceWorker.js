cc42dd91daad037f3423ac5446d21a3ad95c5ffe
jflanaganuk <josh@wearejh.com>
2019-04-08 12:51:06 +0100

    Added extra file types to workbox

--- serviceWorker.js
+++ serviceWorker.js
@@ -3,7 +3,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox
 if (workbox) {
 	console.log(`Yay! Workbox is loaded 🎉`);
 	workbox.routing.registerRoute(
-		new RegExp('.*\.js'),
+		/\.(?:png|jpg|jpeg|svg|gif|js|html|css)$/,
 		new workbox.strategies.NetworkFirst()
 	);
 } else {
