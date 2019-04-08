e8370c0511832ef57f55f9e28aaaf7209efeaf6e
jflanaganuk <josh@wearejh.com>
2019-04-08 12:47:58 +0100

    Added js strategy to workbox

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,11 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js');
 
 if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
+	console.log(`Yay! Workbox is loaded 🎉`);
+	workbox.routing.registerRoute(
+		new RegExp('.*\.js'),
+		new workbox.strategies.NetworkFirst()
+	);
 } else {
-  console.log(`Boo! Workbox didn't load 😬`);
+	console.log(`Boo! Workbox didn't load 😬`);
 }
