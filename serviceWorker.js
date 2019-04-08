a5490bea7c47b23548f662443383b05a64875fd8
jflanaganuk <josh@wearejh.com>
2019-04-08 13:00:12 +0100

    Changed approach to sw

--- serviceWorker.js
+++ serviceWorker.js
@@ -3,10 +3,16 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox
 if (workbox) {
 	console.log(`Yay! Workbox is loaded 🎉`);
 	workbox.routing.registerRoute(
-		/\.(?:png|jpg|jpeg|svg|gif|js|html|css)$/,
-		new workbox.strategies.CacheFirst({
-			cacheName: 'offline-cache',
-		})
+	  new RegExp('.*\.js'),
+	  new workbox.strategies.CacheFirst()
+	);
+	workbox.routing.registerRoute(
+	  new RegExp('.*\.css'),
+	  new workbox.strategies.CacheFirst()
+	);
+	workbox.routing.registerRoute(
+	  '/',
+	  new workbox.strategies.CacheFirst()
 	);
 } else {
 	console.log(`Boo! Workbox didn't load 😬`);
